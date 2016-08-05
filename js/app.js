5844d3d6a424ab13e3a47eccff8e94a895ed66a5
jackton1 <jtonye@ymail.com>
2016-08-04 23:18:33 -0400

    Fixed issues with colors payment and total

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
+//In the Job role section reveal a text field when the other section is selected from
+//the job role drop down menu
 var addInputField = function(location, value){
-  //In the Job role section reveal a text field when the other section is selected from
-  //the job role drop down menu
   //Add a text input field
     //Use the id of the "other-title" for the field
     //Add a placeholder text of "Your Title" for the field
@@ -14,42 +14,51 @@ var addInputField = function(location, value){
     }
 }
 
+//Bind the change and click function to the
 $('#title').bind("change click", function(){
   var jobRole = $(this);
   var value = jobRole[0].value;
   addInputField(jobRole, value);
 });
 
-
-//Set focus to the first Text field on load
-$(function(){
-  $('#name').focus();
-  //Hide the color div onload
-  $('#colors-js-puns').css("display","none");
-});
-
 var hideColor = function(length){
   var i;
   for(i = 0 ; i < length ; i++){
     //Hide all colors
-    $('#color option:nth-of-type('+i+')').css("display","none");
+    $('#color option:eq('+ i +')').prop('disabled', true);
   }
+
+  // $('#color option').each(function(){
+  //   $('#color option').each(function(){
+  //     if($(this).is(":disabled")){
+  //        $(this).detach();
+  //     }
+  //     else{
+  //       $(this).appendTo($('#color'));
+  //     }
+  //   });
+  // })
+
 }
 
 var showColor = function(min, max){
   var i;
+  $('#colors-js-puns').css("display","block");
   //Set the option val to the min option
-  $("#color").val($('#color option:nth-of-type('+min+')').val());
+  $("#color").val($('#color option:eq('+min+')').val());
   for(i = min ; i < max ; i++){
     //Show only the color related to the option selected
-    $('#color option:nth-of-type('+i+')').css("display","block");
+    $('#color option:eq('+i+')').prop('disabled', false);
   }
 }
 
-//Hide the color label and the select menu until the user selects a color
+
+
+//Hide the color label and the select menu until the user selects a Design Theme
 $('#design').on("change", function(){
-  var optionValue = $(this).val();
+  var optionValue = this.value;
   var colorOptions = $('#color option');
+  //Store the length all available colors
   var length = colorOptions.length;
 
   switch(optionValue){
@@ -57,19 +66,17 @@ $('#design').on("change", function(){
          $('#colors-js-puns').css("display","none");
          break;
     case "js puns":
-          $('#colors-js-puns').css("display","block");
           //If the user selects Theme-JS Puns then the color menu should only display
           //"Corn flower Blue", "Dark Slate Grey" and "Gold". first 3 colors.
-          min = 1;
+          min = 0;
           max = 3;
           hideColor(length);
           showColor(min, max);
           break;
     case "heart js":
           //If the user select "Theme - 1  JS" then the color menu should only display "Tomato",
-          //"Steel Blue" and "Dim Grey"
-          $('#colors-js-puns').css("display","block");
-          var min = 4;
+          //"Steel Blue" and "Dim Grey last 3 colors"
+          var min = 3;
           var max = colorOptions.length;
           hideColor(length);
           showColor(min, max);
@@ -80,6 +87,70 @@ $('#design').on("change", function(){
 });
 
 
+//When the user chooses any of the activities calculate the total
+var fieldCheckbox = $('.activities label input');
+  fieldCheckbox.on("change", function() {
+    //Store the activity information
+    var activityData;
+    var number = 0;
+    for (var i = 0; i < fieldCheckbox.length; i++) {
+      var chosen = fieldCheckbox[i];
+      if (chosen.checked){
+        activityData = $('.activities label input')[i].nextSibling.textContent.split(/[\s,$]+/);
+        number += parseInt(activityData[activityData.length - 1]);
+        console.log(activityData);
+      }
+    }
+    //Append the Total to the activities fieldset
+    var $total = $('<p id="total">Total: '+ number +'</p>');
+    //Check if the element doesn't exist in the DOM before appending
+    if(!$('#total').get(0)){
+      $('.activities').append($total);
+    }
+    else{
+      //else Update the value
+      $('#total')[0].innerText = 'Total: ' + number;
+      //Check if the total is 0 remove the paragraph tag
+      if(number == 0){
+        $('#total').remove();
+      }
+    }
+  });
+
+
+//
+$('#payment').on("change", function(){
+  //Get the payment value selected
+    var payMethod = this.value;
+    //Check if any of the payment methods where chosen
+    switch (payMethod) {
+      case "credit card":
+           $("#credit-card").show();
+        break;
+      case "paypal":
+          $(this).siblings().hide();
+          $('#payment').siblings()[3].style.display = "block";
+        break;
+      case "bitcoin":
+         $(this).siblings().hide();
+         $('#payment').siblings()[4].style.display = "block";
+       break;
+      default:
+          $(this).siblings().hide();
+    }
+});
+
+
+
+
+$(function(){
+  //Set focus to the first Text field on load
+  $('#name').focus();
+  //Hide the color div onload
+  $('#colors-js-puns').css("display","none");
+  //Hide all payment methods on load
+  $('#payment').siblings().hide();
+});
 
 
 
