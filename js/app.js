5105714c56f263210094a8874ade083b2f67fc5d
jackton1 <jtonye@ymail.com>
2016-08-04 23:26:40 -0400

    Modified comments

--- js/app.js
+++ js/app.js
@@ -141,8 +141,6 @@ $('#payment').on("change", function(){
 });
 
 
-
-
 $(function(){
   //Set focus to the first Text field on load
   $('#name').focus();
@@ -157,18 +155,15 @@ $(function(){
 /*Register for Activities section of the form.
 Some events are at the same time as others.
 If the user selects a workshop, don't allow selection of a workshop at the same date and time
- -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
-When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
-As a user selects activities to register for, a running total is listed below the list of checkboxes.
- For example, if the user selects "Main conference" then Total: $200 should appear.
-  If they add 1 workshop the total should change to Total: $300.
-Payment Info section of the form. Display payment sections based on chosen payment option
-The"Credit Card" payment option should be selected by default and result in the display of the #credit-card div,
-and hide the "Paypal" and "Bitcoin information.
-When a user selects the "PayPal" payment option, display the Paypal information,
-and hide the credit card information and the "Bitcoin" information.
-When a user selects the "Bitcoin" payment option, display the Bitcoin information,
-and hide the credit card information.
+ -- you should disable the checkbox and visually indicate that the workshop in the competing time slot
+ isn't available.
+When a user unchecks an activity, make sure that competing activities (if there are any)
+are no longer disabled.
+
+
+
+
+
 Form validation. Display error messages and don't let the user submit the form
 if any of these validation errors exist:
 Name field can't be empty
